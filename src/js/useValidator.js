3e6707cce7d7b3d2409c2bd2d8e14e543eddcc4f
Idris Dopico Peña <dopico1996@hotmail.com>
2023-02-03 12:06:33 +0100

    Final version before time

--- src/js/useValidator.js
+++ src/js/useValidator.js
@@ -1,11 +1,10 @@
-export const useValidator = (schema, objToValidate) => {
+export const useValidator = (schema, objectToValidate) => {
   for (let prop in schema) {
-    // Valideer de type van het object, vergeleken met het type in het schema
-    if (typeof objToValidate[prop] !== schema[prop]) {
-
-      // Check of het type van de prop een object of Array is
-      if (typeof objToValidate[prop] === 'object') {
-        return Array.isArray(objToValidate[prop]);
+    // Check type van het object, vergeleken met het type in het schema
+    if (typeof objectToValidate[prop] !== schema[prop]) {
+      // Check of de prop het type "object" heeft
+      if (typeof objectToValidate[prop] === 'object') {
+        return objectToValidate[prop].constructor === Array && schema[prop] === 'array';
       }
 
       return false;
@@ -14,3 +13,26 @@ export const useValidator = (schema, objToValidate) => {
 
   return true;
 }
+
+
+// @TODO: Dit is even om de gedachtengang te laten zien aan jullie :)
+// typeof objToValidate[prop] === object / array
+// objectToValidate[prop].constructor === Array
+
+// export const useValidator = (schema, objectToValidate) => {
+//   for (let prop in schema) {
+//     console.log(objectToValidate[prop], 'should be: ', schema[prop]);
+//
+//     // Valideer de type van het object, vergeleken met het type in het schema
+//     if (typeof objectToValidate[prop] !== schema[prop]) {
+//       return false;
+//     }
+//
+//     return objectToValidate[prop].constructor === Array && schema[prop] === 'array';
+//   }
+//
+//   return true;
+// }
+
+// typeof objToValidate[prop] === object / array
+// objectToValidate[prop].constructor === Array
